8ec0a0d61fa5b66cc6d504baff5b2f878ee86d4b
Andrew J Vargo <ajvargo@computer.org>
2010-10-20 23:00:08 -0400

    Added posts controller with fleshed out methods; added blank views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  respond_to :html
+
+  def show
+    @posts = Post.find params[:id]
+  end
+
+  def new
+    @post = Post.new
+    @post.category = Category.new
+  end
+
+  def create
+    category = Category.find_by_category params["post"]["category_attributes"]["name"]
+    if category
+      params["post"]["category_id"] = category.id
+      params["post"].delete "category_attributes"
+    end
+    @post = Post.new params["post"]
+    @post.save
+    respond_with(@post)
+  end
+end
